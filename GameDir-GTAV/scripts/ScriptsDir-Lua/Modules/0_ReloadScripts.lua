42e092400b980da8413c6c211165d20a2c367807
Jasen Samuels <jasensamuels@gmail.com>
2022-08-05 04:20:59 -0400

    Update 0_ReloadScripts.lua

--- GameDir-GTAV/scripts/ScriptsDir-Lua/Modules/0_ReloadScripts.lua
+++ GameDir-GTAV/scripts/ScriptsDir-Lua/Modules/0_ReloadScripts.lua
@@ -1,14 +1,24 @@
 if DebugMode then
-	local wait, Scripts_Init, print, IsControlPressed, get_key_pressed
-		= wait, Scripts_Init, print, IsControlPressed, get_key_pressed
-	return {
-		loop	=	function()
-						--[[If Tab Key Pressed and not Alt Key Pressed then]]
-						if get_key_pressed(9) and not IsControlPressed(0, 19) then
-							print('Reloading Scripts')
-							Scripts_Init()
-							wait(2499)
-						end
-					end
-	}
+	JM36.CreateThread(function()
+		local Info = Info
+		local yield = JM36.yield
+		
+		if ReloadScripts then
+			yield(ReloadScripts - Info.Time)
+			ReloadScripts = nil
+		end
+		
+		local Scripts_Init, print, IsControlPressed, get_key_pressed
+			= Scripts_Init, print, IsControlPressed, get_key_pressed
+		
+		while true do
+			--[[If Tab Key Pressed and not Alt Key Pressed then]]
+			if get_key_pressed(9) and not IsControlPressed(0, 19) then
+				ReloadScripts = Info.Time + 2500
+				print"Reloading Scripts"
+				Scripts_Init()
+			end
+			yield()
+		end
+	end)
 end
