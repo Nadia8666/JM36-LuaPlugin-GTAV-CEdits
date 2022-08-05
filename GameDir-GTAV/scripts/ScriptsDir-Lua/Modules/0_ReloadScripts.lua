7860d5973cb8d468bbd216ac43d1e70a7c451f7b
Jasen Samuels <jasensamuels@gmail.com>
2022-08-05 05:38:40 -0400

    Update 0_ReloadScripts.lua

--- GameDir-GTAV/scripts/ScriptsDir-Lua/Modules/0_ReloadScripts.lua
+++ GameDir-GTAV/scripts/ScriptsDir-Lua/Modules/0_ReloadScripts.lua
@@ -1,14 +1,12 @@
 if DebugMode and not _G2.ReloadScripts then _G2.ReloadScripts = true
 	JM36.CreateThread_HighPriority(function()
-		local Info = Info
+		local Scripts_Init, print = Scripts_Init, print
+		
+		local ShouldReload = require"0_ReloadScriptsTrigger"
+		
 		local yield = JM36.yield
-		
-		local Scripts_Init, print, IsControlPressed, get_key_pressed
-			= Scripts_Init, print, IsControlPressed, get_key_pressed
-		
 		while true do
-			--[[If Tab Key Pressed and not Alt Key Pressed then]]
-			if get_key_pressed(9) and not IsControlPressed(0, 19) then
+			if ShouldReload() then
 				print"Reloading Scripts"
 				Scripts_Init()
 				yield(2499)
