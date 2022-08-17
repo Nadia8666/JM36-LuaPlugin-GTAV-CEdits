983ea7b38db23d343d2001bfc879536f41823adf
Jasen Samuels <jasensamuels@gmail.com>
2022-08-17 15:22:12 -0400

    Update ExampleBase.lua

--- GameDir-GTAV/scripts/ScriptsDir-Lua/Modules/ExampleBase.lua
+++ GameDir-GTAV/scripts/ScriptsDir-Lua/Modules/ExampleBase.lua
@@ -1,4 +1,30 @@
 --[[
+
+-- Modern Examples/Stuff
+
+
+-- Creates a thread, prints "Hello World!" once, and then terminates thread
+JM36.CreateThread(function()
+	print("Hello World!")
+end)
+
+
+-- Creates a thread, prints "Hello World!" forever (for as long as that thread is running for)
+JM36.CreateThread(function()
+	while true do
+		JM36.Wait(0)
+	end
+end)
+
+]]
+
+
+
+--[[
+
+-- Legacy Examples/Stuff
+
+
 return {
 	init	=	nil or function()
 					--Stuff that runs only once, on startup of the script
@@ -10,4 +36,5 @@ return {
 					--Stuff that runs only once, on shutdown of the script
 				end,
 }
+
 ]]
