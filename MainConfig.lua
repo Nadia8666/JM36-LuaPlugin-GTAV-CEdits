0484391742a96c0ca3c202b112a3c1438023a482
Nadia8666 <chocklet.cats@gmail.com>
2022-12-12 11:42:37 -0800

    New Script Reload System No longer requires reload of script

---
+++ MainConfig.lua
@@ -0,0 +1,10 @@
+-- Configuration system test, removes the need to restart the game for main config changes
+local MainConfig = {
+
+    DebugMode = true, -- Enable script reloading with the script reload key (default TAB), Usage: DebugMode = value,
+
+}
+
+
+
+return MainConfig
--- Modules/0_ReloadScripts.lua
+++ Modules/0_ReloadScripts.lua
@@ -1,16 +1,19 @@
-if DebugMode and not ScriptsReloadTriggerLoaded then ScriptsReloadTriggerLoaded = true
+local Config = configFileRead("MainConfig.ini")
+if not ScriptsReloadTriggerLoaded then ScriptsReloadTriggerLoaded = true
 	JM36.CreateThread_HighPriority(function()
 		local Scripts_Init, print = Scripts_Init, print
 		
 		local ShouldReload = require"0_ReloadScriptsTrigger"
+		local MConf = require("MainConfig.lua")
 		
 		local yield = JM36.yield
 		while true do
-			if ShouldReload() then
+			if ShouldReload() and MConf.DebugMode == false then
 				print"Reloading Scripts"
 				Scripts_Init()
 				yield(2499)
 			end
+			unrequire("MainConfig.lua")
 			yield()
 		end
 	end)
