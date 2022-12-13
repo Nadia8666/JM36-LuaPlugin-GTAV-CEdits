00f11808fadd10d68c6f39219f01909cc4dbac98
Nadia <115858781+Nadia8666@users.noreply.github.com>
2022-12-13 03:24:57 -0800

    Delete __Modules directory

--- __Modules/0_ReloadScriptsTrigger.lua
+++
@@ -1,5 +0,0 @@
-local IsControlPressed = IsControlPressed
-return function()
-	--[[If Tab Key Pressed and not Alt Key Pressed then]]
-	return IsControlPressed(0, 37) and not IsControlPressed(0, 19)
-end
