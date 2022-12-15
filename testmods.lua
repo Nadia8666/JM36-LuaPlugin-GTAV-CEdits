40d1c2b87ecb8b894298c1dc4a441a564d1cc727
Nadia8666 <chocklet.cats@gmail.com>
2022-12-15 00:54:11 -0800

    update to lastest code version (dev-staged main repo)

--- testmods.lua
+++ testmods.lua
@@ -1,33 +1,47 @@
 -- file used when working on code changes for -master branch--
-
-local function clamp(num, min, max) -- for some reason lua doesnt have math.clamp as a default function and it makes me sad :( its very useful
-    if num > max then num = max elseif num < min then num = min end -- num < min = min | num > max = max
-    return num
-end
-
-function configFileFindLineFromText(file, text, maxOccurences)
-    maxOccurences = clamp(maxOccurences or 1, 1, math.huge) -- by default return first | 0 and 1 would act the same but 1 is easier to read imo due to table index's starting with 1 and everything
-
-    local filePath = Scripts_Path.. file
-    local configFile = assert(io_open(filePath, "r"), "Invalid File Path")
-    local lines = {}
-
-    for L in configFile:lines() do
-        -- Loop through every line
-        table_insert(lines, L)
-    end
-    configFile:close()
-
-    local returnLineNum
-    local occurences = 0
-
-    for i,v in ipairs(lines) do
-        if v:find(text) then
-            occurences = occurences + 1
-            returnLineNum = i 
-            if occurences >= maxOccurences then break end
-        end
-    end
-
-    return returnLineNum -- nil | number
+local FunctionForOccurenceType = setmetatable({
+	["nil"] = function(configFile, text --[[, occurence]])
+		local RetVal
+		for line in configFile:lines() do
+			if line:find(text) then
+				RetVal = line
+			end
+		end
+		return RetVal
+	end,
+	["number"] = function(configFile, text, occurence)
+		local RetVal
+		local OccurenceCurrent = 0
+		for line in configFile:lines() do
+			if line:find(text) then
+				RetVal = line
+				OccurenceCurrent = OccurenceCurrent + 1
+				if OccurenceCurrent == occurence then
+					break
+				end
+			end
+		end
+		return RetVal, OccurenceCurrent ~= occurence and OccurenceCurrent -- Second return is truthy "failed"; second return will be "false" if we found and returned the requested occurence, otherwise will be the occurence number.
+	end,
+	["boolean"] = function(configFile, text --[[, occurence]])
+		local RetVal = {}
+		local OccurenceCurrent = 0
+		for line in configFile:lines() do
+			if line:find(text) then
+				OccurenceCurrent = OccurenceCurrent + 1
+				RetVal[OccurenceCurrent] = line
+			end
+		end
+		return RetVal
+	end},
+    {
+	__index = function(Self --[[, Key]])
+		return Self["nil"]
+	end,
+})
+function configFileFindLineFromText(file, text, occurence)
+	local configFile = assert(io_open(Scripts_Path .. file, "r"), "Invalid File Path")
+	local line, failed = FunctionForOccurenceType[type(occurence)](configFile, text, occurence)
+	configFile:close()
+	return line, failed
 end
